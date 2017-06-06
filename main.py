0fd3062526cc47e0f47caa96f4bd8863cf231d4b
Martino Mensio <martinomensio@outlook.it>
2017-06-06 16:34:49 +0200

    info string + area entity

--- main.py
+++ main.py
@@ -8,69 +8,88 @@ from pprint import pprint
 import wit_client
 import polito_client
 
+examples = '_Quali aule libere ci sono ora?_\n_Domani alle 10 dove posso andare a studiare?_\n' + \
+    '\nPuoi specificare un riferimento temporale e un riferimento spaziale:\n-temporale: abbastanza generico, dovrei capirti comunque\n-spaziale: puoi dirmi di mostrarti solo alcuni risultati in una certa area del poli (ad esempio "cittadella politecnica" o altri valori che puoi vedere in un risultato non filtrato)\n' + \
+    '\n_Fra due ore quali aule sono libere al lingotto?_\n' + \
+    'Scrivi la domanda in modo naturale'
+
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
     if content_type == 'text':
-        intent, entities = ent_extractor.parse(msg['text'])
-
-        pprint(intent)
-        pprint(entities)
-        if intent:
-            if intent['value'] == 'greetings':
-                bot.sendMessage(chat_id, 'ciao!')
-
-            elif intent['value'] == 'info':
+        if msg['text'].startswith('/'):
+            # this is a command
+            if msg['text'] == '/start':
                 bot.sendMessage(
-                    chat_id, 'chiedimi informazioni sulle aule libere')
+                    chat_id, 'Benvenuto! chiedimi informazioni sulle aule libere')
+            elif msg['text'] == '/help':
+                bot.sendMessage(
+                    chat_id, 'Eccoti alcuni esempi di utilizzo:\n' + examples, parse_mode="Markdown")
+        else:
+            intent, entities = ent_extractor.parse(msg['text'])
 
-            elif intent['value'] == 'search_rooms':
-                datetime = entities.get('datetime')
-                location = entities.get('location')
-                loading_string = 'Sto cercando aule '
-                if datetime:
-                    if not datetime.get('value'):
-                        # if this is an interval
-                        datetime = datetime['from']
-                    date_param = datetime['value'].split('T')[0]
-                    time_param = datetime['value'].split('T')[1].split('+')[0]
-                    loading_string += ' il giorno ' + date_param + ' ora ' + time_param
+            # pprint(intent)
+            # pprint(entities)
+            if intent:
+                if intent['value'] == 'greetings':
+                    bot.sendMessage(chat_id, 'ciao!')
+
+                elif intent['value'] == 'info':
+                    bot.sendMessage(
+                        chat_id, 'chiedimi informazioni sulle aule libere. Esempi:\n' + examples, parse_mode="Markdown")
+
+                elif intent['value'] == 'search_rooms':
+                    datetime = entities.get('datetime')
+                    area = entities.get('area')
+                    loading_string = 'Sto cercando aule '
+                    if datetime:
+                        if not datetime.get('value'):
+                            # if this is an interval
+                            datetime = datetime['from']
+                        date_param = datetime['value'].split('T')[0]
+                        time_param = datetime['value'].split('T')[
+                            1].split('+')[0]
+                        loading_string += ' il giorno ' + date_param + ' ora ' + time_param
+
+                    else:
+                        date_param = None
+                        time_param = None
+
+                    if area:
+                        loading_string += ' in ' + area['value']
+
+                    bot.sendMessage(chat_id, loading_string)
+
+                    result = data_provider.getRooms(
+                        {'date': date_param, 'time': time_param})
+                    # pprint(result)
+                    all_rooms = result['aule_libere']
+                    for curr_area, rooms in all_rooms.items():
+                        # key is an area, value a list of rooms
+                        if area:
+                            delimiters = [' ', '_', ',', ', ', '-', '\n']
+                            regex_pattern = '|'.join(
+                                map(re.escape, delimiters))
+                            search_filters = re.split(
+                                regex_pattern, area['value'])
+                            if not all(filter in curr_area.lower() for filter in search_filters):
+                                continue
+
+                        res = ''
+                        for room in rooms:
+                            res += '    ' + room['nome_aula']
+
+                        bot.sendMessage(chat_id, curr_area + ': ' + res)
 
                 else:
-                    date_param = None
-                    time_param = None
-
-                if location:
-                    loading_string += ' in ' + location['value']
-
-                bot.sendMessage(chat_id, loading_string)
-
-                result = data_provider.getRooms(
-                    {'date': date_param, 'time': time_param})
-                # pprint(result)
-                all_rooms = result['aule_libere']
-                for area, rooms in all_rooms.items():
-                    # key is an area, value a list of rooms
-                    if location:
-                        delimiters = [' ', '_', ',', ', ', '-', '\n']
-                        regex_pattern = '|'.join(map(re.escape, delimiters))
-                        search_filters = re.split(regex_pattern, location['value'])
-                        if not all(filter in area.lower() for filter in search_filters):
-                            continue
-
-                    res = ''
-                    for room in rooms:
-                        res += '    ' + room['nome_aula']
-
-                    bot.sendMessage(chat_id, area + ': ' + res)
+                    bot.sendMessage(chat_id, 'intento ' + intent['value'])
 
             else:
-                bot.sendMessage(chat_id, 'intento ' + intent['value'])
-
-        else:
-            bot.sendMessage(chat_id, 'non ti capisco')
+                bot.sendMessage(chat_id, 'non ti capisco')
+    else:
+        bot.sendMessage(chat_id, 'non supporto ancora questo tipo di messaggi')
 
 
 bot = telepot.Bot(os.environ['TELEGRAM_TOKEN'])
